73a3d9008c8b9d82f07833033da8cf65801495c3
O'Neil Obidiaso <obidiasodumebi@gmail.com>
2025-06-16 21:16:02 +0100

    linked course cta to courses section

--- src/components/Hero/CTA_Buttons.jsx
+++ src/components/Hero/CTA_Buttons.jsx
@@ -1,16 +1,14 @@
-
-
 const CTA_Buttons = () => {
   return (
     <div className="flex flex-wrap justify-center gap-4 mt-8">
-          <button className="px-8 py-3 bg-[var(--primary)] text-white font-medium rounded-lg transition-all transform hover:scale-105 shadow-lg cursor-pointer">
-            Get Started
-          </button>
-          <button className="px-8 py-3 border-2 border-primary text-primary hover:bg-primary/10 font-medium rounded-lg transition-all transform hover:scale-105 cursor-pointer">
-            Explore Courses
-          </button>
-        </div>
-  )
-}
+      <button className="px-8 py-3 bg-[var(--primary)] text-white font-medium rounded-lg transition-all transform hover:scale-105 shadow-lg cursor-pointer">
+        Get Started <a href=""></a>
+      </button>
+      <button className="px-8 py-3 border-2 border-primary text-primary hover:bg-primary/10 font-medium rounded-lg transition-all transform hover:scale-105 cursor-pointer">
+        <a href="#courses">Explore Courses</a>
+      </button>
+    </div>
+  );
+};
 
-export default CTA_Buttons
+export default CTA_Buttons;
